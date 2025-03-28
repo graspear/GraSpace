dab3bc651774b212cae659fe473e9d57b8d77d3f
PremKumarAddala <premkumar@gramatrix.in>
2025-03-28 17:43:30 +0530

    Updated the code

--- coreplugins/cesiumion/public/defaults/index.jsx
+++
@@ -1,5 +0,0 @@
-import AssetType from "./AssetType";
-import SourceType from "./SourceType";
-import AssetConfig from "./AssetConfig";
-
-export { AssetType, SourceType, AssetConfig };
--- coreplugins/dronedb/README.md
+++
@@ -1,5 +0,0 @@
-<h1 align="center">DroneDB</h1>
-
-DroneDB is a WebODM add-on that enables you to import and export your files to DroneDB.
-
-
